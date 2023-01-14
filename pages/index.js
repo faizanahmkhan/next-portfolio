9f38d9a761421a2f3021e71737401c589d5354e6
faizanahmkhan <faizanahmkhan@gmail.com>
2023-01-14 18:14:22 +0000

    further formatting

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
   return (
     <div className={darkMode ? "dark" : ""}>
       <Head>
-        <title>Faizan Khan's Porfolio</title>
+        <title>Faizan Khan's Portfolio</title>
         <meta name="description" content="Welcome to my portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
